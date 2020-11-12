1be429db422a1c909a11787fe3d13c9668baa094
Rubén Guerrero <yo@rubenguerrero.com>
2020-11-12 16:41:25 +0100

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from "react";
+import { Router, Link, useParams } from "@reach/router";
 
 import logo from "./logo.svg";
 import "./App.css";
 
-function App({ app }) {
-  return (
+const Home = () => (
+  <div>
+    <h1>Home</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">Home</Link> | <Link to="dashboard">Dashboard failing</Link>{" "}
+        | <Link to="dashboard/1">Dashboard</Link>
       </header>
+    </div>
+  </div>
+);
+
+const Dash = () => {
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>Dashboard {params.id.toString()}</h1>
+    </div>
+  );
+};
+
+function App({ app }) {
+  return (
+    <div className="App">
+      <Router>
+        <Home path="/" />
+        <Dash path="dashboard/:id" />
+      </Router>
     </div>
   );
 }
