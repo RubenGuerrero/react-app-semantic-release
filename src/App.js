2bc2ca63337efa18f045a0936fa5d2d19856c303
Rubén Guerrero <yo@rubenguerrero.com>
2020-11-12 17:26:33 +0100

    feat: refactor

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from "react";
-import { Router, Link, useParams } from "@reach/router";
+import { Router } from "@reach/router";
 
-import logo from "./logo.svg";
+import { Home, Dash } from "./modules";
+
 import "./App.css";
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World</h1>
-        <Link to="/">Home</Link> | <Link to="dashboard">Dashboard failing</Link>{" "}
-        | <Link to="dashboard/1">Dashboard</Link>
-      </header>
-    </div>
-  </div>
-);
-
-const Dash = () => {
-  const params = useParams();
-
-  return (
-    <div>
-      <h1>Dashboard {params.id.toString()}</h1>
-    </div>
-  );
-};
-
-function App({ app }) {
+function App() {
   return (
     <div className="App">
       <Router>
